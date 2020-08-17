36ead8b1425137be2aada728d4e5b20d2fc2d29d
Reda <ghalou.reda@gmail.com>
2020-08-17 10:38:15 +0100

    First Commit - Sport Coaching

---
+++ src/com/luv2code/springdemo/FootballCoach.java
@@ -0,0 +1,16 @@
+package com.luv2code.springdemo;
+
+public class FootballCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Play a 1 hour match, 6 vs 6";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
